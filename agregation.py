2364643bca4152e8d2d3d56f0140f10e80dd156d
Souad-djerfi <bensaad.souad@gmail.com>
2023-04-16 22:15:14 +0200

    test pull request

--- agregation.py
+++ agregation.py
@@ -4,7 +4,7 @@ from flask import jsonify
 import statistics
 
 def agre(type, start, end, data):
-    print(type)
+    
     #convertir les dates start et end ( UNIX) en  datetime
     data_final=[]
     start_dt=datetime.fromtimestamp(int(start))
@@ -12,7 +12,7 @@ def agre(type, start, end, data):
 
     # tester si l'interval entre start_dt et end_dt > 24 
     if (end_dt-start_dt).total_seconds()/3600 > 24:
-        
+
     #calculer nombre d'internval de 2h
         nbr_interval=(end_dt - start_dt) // timedelta(hours=2)
         for i in range(nbr_interval) :
